a1a2ae87f2528a989151f26c9023074bcb284a93
soaalice <amboararampanana@gmail.com>
2026-02-07 18:19:59 +0300

    fix styles

--- src/app/shared/components/modal-forms/modal-forms.component.ts
+++ src/app/shared/components/modal-forms/modal-forms.component.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-modal-forms',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   templateUrl: './modal-forms.component.html',
   styleUrl: './modal-forms.component.scss'
 })
